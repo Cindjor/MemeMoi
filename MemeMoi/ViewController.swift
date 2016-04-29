dfc45bf9ad6e24b273e9618f2839bea30eff349d
Cindjor <cheung.luke@gmail.com>
2016-04-28 22:42:44 -0700

    basic
    need to update to newest Swift version

---
+++ MemeMoi/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  MemeMoi
+//
+//  Created by Luke CHEUNG on 3/11/16.
+//  Copyright © 2016 Luke Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.topTextField.text = "Type text here"
+        self.bottomTextField.text = "Tap here to edit"
+        topTextField.textAlignment = .Center;
+        bottomTextField.textAlignment = .Center;
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+    }
+    
+    
+    @IBOutlet var bottomTextField: UITextField!
+    @IBOutlet var topTextField: UITextField!
+    @IBOutlet weak var cameraButton: UIButton!
+
+    @IBAction func pickImageCamera(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImage(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+        func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+                imagePickerView.image
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+        
+    }
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName : UIColor.whiteColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : 40
+    ]
+    
+    
+    
+    
+    @IBAction func topTextField(sender: AnyObject) {
+        
+    }
+    @IBAction func bottomTextField(sender: AnyObject) {
+    }
+    func keyboardwillshow() {
+        
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:))    , name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    func getKeyboardHeight(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        let keyboardSize = userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unsubscribeFromKeyboardNotifications()
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        self.subscribeToKeyboardNotifications()
+        
+    }
+    
+
+
+}
+
